22f18a63b7d1dc36585e46524c8fc3548acefdeb
Hans <hanscastroadmn@gmail.com>
2022-03-22 18:22:15 +0100

    [Domain] Added method on contract

--- src/Domain/Reservation/Contracts/IReservationRepository.php
+++ src/Domain/Reservation/Contracts/IReservationRepository.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Src\Domain\Reservation\Contracts;
 
+use Src\Domain\Reservation\Properties\ReservationId;
 use Src\Domain\Reservation\Reservation;
 
 interface IReservationRepository
 {
 
-    function getReservationById(): Reservation;
+    function getReservationById(ReservationId $reservationId): Reservation;
 
-
-
+    function createReservation(Reservation $reservation): Reservation;
 }
