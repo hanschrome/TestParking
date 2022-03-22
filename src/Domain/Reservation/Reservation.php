29d7dd818fa5ca6846a9b8ab742f6449d4148040
Hans <hanscastroadmn@gmail.com>
2022-03-22 18:18:51 +0100

    [Domain] Added ReservationId on main model Reservation

--- src/Domain/Reservation/Reservation.php
+++ src/Domain/Reservation/Reservation.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Src\Domain\Reservation;
 
+use Src\Domain\Reservation\Properties\ReservationId;
+
 class Reservation
 {
-
+    public ReservationId $reservationId;
 }
